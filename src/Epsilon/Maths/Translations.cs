5cfbc00891dfcbe16de1213564c75d36897b02d0
Stevö John <stevehuwjohn@gmail.com>
2019-09-18 15:35:13 +0100

    Resharpering

--- src/Epsilon/Maths/Translations.cs
+++ src/Epsilon/Maths/Translations.cs
@@ -6,7 +6,7 @@ namespace Epsilon.Maths
     {
         public static Coordinates BoardToScreen(int x, int y)
         {
-            var sx = Constants.ScreenBufferWidth / 2 + (x - y) * (Constants.TileSpriteWidthHalf) - Constants.TileSpriteWidthHalf;
+            var sx = Constants.ScreenBufferWidth / 2 + (x - y) * Constants.TileSpriteWidthHalf - Constants.TileSpriteWidthHalf;
 
             var sy = Constants.YScreenOffset + y * Constants.TileHeightHalf + x * Constants.TileHeightHalf;
 
--- src/Epsilon/Properties/AssemblyInfo.cs
+++ src/Epsilon/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -9,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Epsilon")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Outside Context Problem")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
