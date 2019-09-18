02aca3ab42bf436837c7678d87928639b46eecf8
Stevö John <stevehuwjohn@gmail.com>
2019-09-18 01:56:22 +0100

    -

--- src/Epsilon/Maths/Translations.cs
+++ src/Epsilon/Maths/Translations.cs
@@ -1,10 +1,16 @@
-﻿namespace Epsilon.Maths
+﻿using Epsilon.Infrastructure;
+
+namespace Epsilon.Maths
 {
     public static class Translations
     {
         public static Coordinates BoardToScreen(int x, int y)
         {
-            return null;
+            var sx = Constants.ScreenBufferWidth / 2 + x * Constants.TileSpriteWidth - Constants.ScreenBufferWidth / 2;
+
+            var sy = 0;
+
+            return new Coordinates(sx, sy);
         }
     }
 }
