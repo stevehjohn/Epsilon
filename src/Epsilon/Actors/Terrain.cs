8ad556abf7c4f98dce0be79b6d6a491ab3d1b7c7
Steve John <stevehuwjohn@gmail.com>
2019-09-24 17:52:23 +0100

    WIP - fixing sky

--- src/Epsilon/Actors/Terrain.cs
+++ src/Epsilon/Actors/Terrain.cs
@@ -108,11 +108,11 @@ namespace Epsilon.Actors
                         baseHeight = tile.Height;
                     }
 
-                    if (x == 0 || y == 0)
+                    if (x == 0 || y == 0 || _map.GetTile(x - 1, y) == null || _map.GetTile(x - 1, y) == null)
                     {
                         var skyBase = position.Y - Constants.SkySpriteHeight - Constants.SeaFloor * Constants.BlockHeight + Constants.TileHeightHalf;
 
-                        if (x == 0)
+                        if (x == 0 || _map.GetTile(x - 1, y) == null)
                         {
                             _spriteBatch.Draw(_sky,
                                               new Vector2(position.X, skyBase),
@@ -122,7 +122,7 @@ namespace Epsilon.Actors
 
                         _depth += Constants.DepthIncrement;
 
-                        if (y == 0)
+                        if (y == 0 || _map.GetTile(x, y - 1) == null)
                         {
                             _spriteBatch.Draw(_sky,
                                               new Vector2(position.X, skyBase),
