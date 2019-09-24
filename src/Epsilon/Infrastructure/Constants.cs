bfb07fd602624cc392c7f0a0bb0e1d868d109b99
Stevö John <stevehuwjohn@gmail.com>
2019-09-24 22:40:09 +0100

    Nice

--- src/Epsilon/Infrastructure/Constants.cs
+++ src/Epsilon/Infrastructure/Constants.cs
@@ -32,7 +32,7 @@
         public const float RadiansHighResolution = 0.00001f;
 
         public const int BoardSize = 38;
-        public const int BoardOverrun = 20;
+        public const int BoardOverrun = 15;
 
         public const float DepthIncrement = 0.00001f;
 
