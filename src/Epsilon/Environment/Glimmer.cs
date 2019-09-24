067b53d6ad9ca48b0f4f163b5d0e5e6ea20f42b7
Stevö John <stevehuwjohn@gmail.com>
2019-09-24 22:57:22 +0100

    WIP

---
+++ src/Epsilon/Environment/Glimmer.cs
@@ -0,0 +1,17 @@
+﻿using Epsilon.Maths;
+
+namespace Epsilon.Environment
+{
+    public class Glimmer
+    {
+        public int Alpha { get; set; }
+
+        public int AlphaDelta { get; set; }
+
+        public Coordinates BoardPosition { get; set; }
+
+        public float YOffset { get; set; }
+
+        public float Velocity { get; set; }
+    }
+}
