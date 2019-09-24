b33b62b4246ce60e97b68b5312cde201cef8efb4
Stevö John <stevehuwjohn@gmail.com>
2019-09-24 01:43:50 +0100

    WIP

--- src/Epsilon/Environment/Map.cs
+++ src/Epsilon/Environment/Map.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Epsilon.Infrastructure;
 using Epsilon.Maths;
 using Epsilon.State;
@@ -182,8 +184,6 @@ namespace Epsilon.Environment
                     var x = (int) (Constants.MapSizeHalf + radius * Math.Sin(radians));
                     var y = (int) (Constants.MapSizeHalf + radius * Math.Cos(radians));
 
-                    Console.WriteLine($"{x}, {y}");
-
                     if (x >= 0 && x < Constants.MapSize && y >= 0 && y < Constants.MapSize)
                     {
                         _tiles[x, y] = null;
@@ -196,8 +196,6 @@ namespace Epsilon.Environment
                 var x = (int) (Constants.MapSizeHalf + Constants.MapSizeHalf * Math.Sin(radians));
                 var y = (int) (Constants.MapSizeHalf + Constants.MapSizeHalf * Math.Cos(radians));
 
-                Console.WriteLine($"{x}, {y}");
-
                 if (x >= 0 && x < Constants.MapSize && y >= 0 && y < Constants.MapSize)
                 {
                     _tiles[x, y] = new Tile(-1, TerrainType.Rock)
@@ -205,6 +203,30 @@ namespace Epsilon.Environment
                                        IsEdge = true
                                    };
                 }
+            }
+
+            var visited = new List<Coordinates>();
+
+            var i = 0;
+
+            for (var radius = 0; radius < Constants.MapSizeHalf; radius++)
+            {
+                for (var radians = 0.0f; radians < Math.PI * 2; radians += (float) (Math.PI * 2 / (radius * 4)))
+                {
+                    var x = (int) (Constants.MapSizeHalf + radius * Math.Sin(radians));
+                    var y = (int) (Constants.MapSizeHalf + radius * Math.Cos(radians));
+
+                    if (x >= 0 && x < Constants.MapSize && y >= 0 && y < Constants.MapSize)
+                    {
+                        if (! visited.Any(c => c.X == x && c.Y == y))
+                        {
+                            _tiles[x, y].Height -= i;
+                            visited.Add(new Coordinates(x, y));
+                        }
+                    }
+                }
+
+                i++;
             }
         }
 
